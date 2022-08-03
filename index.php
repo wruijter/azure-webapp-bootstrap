213e85c9fa933624760b93676b446f57dbd77e2b
wruijter <wruijter@users.noreply.github.com>
2022-08-03 18:06:36 +0200

    Update index.php
    Added a check statement for $_SERVER['HTTP_X_MS_TOKEN_AAD_ID_TOKEN']

--- index.php
+++ index.php
@@ -7,40 +7,48 @@
 			<?php
 				//Check if a valid user is logged in on the Azure Web App configured authentication, https://docs.microsoft.com/nl-nl/azure/app-service/overview-authentication-authorization
 				if(isset($_SERVER['HTTP_X_MS_CLIENT_PRINCIPAL_NAME'])){
-
-					// Find Approles related to the user
-					$tokendata = base64_decode(explode(".",$_SERVER['HTTP_X_MS_TOKEN_AAD_ID_TOKEN'])[1]); //https://stackoverflow.com/questions/58105561/get-assigned-roles-in-azure-web-app-with-php
-					$tokendataArray = json_decode($tokendata); //JSON string to PHP object
-					$rolesAssigned = $tokendataArray->{'roles'}; //https://github.com/MicrosoftDocs/azure-docs/blob/main/articles/active-directory/develop/howto-add-app-roles-in-azure-ad-apps.md
 					
-					// connect to database
-					$con = mysqli_init();
-					mysqli_ssl_set($con,NULL,NULL, "./cert/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-					mysqli_real_connect($con, "xxxx.mysql.database.azure.com", "username", "password", "database", 3306, MYSQLI_CLIENT_SSL);
-					
-					// Do Some MySQL stuff if neccesery, my prefference is to do this in the included PHP files but to start the main connection here (managed from 1 place)
-					//$result = mysqli_query($con,"SELECT * FROM `klanten`");
-					//while ($row = mysqli_fetch_assoc($result)) {
-					//	echo $row['KLANTEN'];
-					//}
+					// App registration is OK and token is available
+					if(isset($_SERVER['HTTP_X_MS_TOKEN_AAD_ID_TOKEN']))){
+						
+						// Find Approles related to the user
+						$tokendata = base64_decode(explode(".",$_SERVER['HTTP_X_MS_TOKEN_AAD_ID_TOKEN'])[1]); //https://stackoverflow.com/questions/58105561/get-assigned-roles-in-azure-web-app-with-php
+						$tokendataArray = json_decode($tokendata); //JSON string to PHP object
+						$rolesAssigned = $tokendataArray->{'roles'}; //https://github.com/MicrosoftDocs/azure-docs/blob/main/articles/active-directory/develop/howto-add-app-roles-in-azure-ad-apps.md
+						
+						// connect to database
+						$con = mysqli_init();
+						mysqli_ssl_set($con,NULL,NULL, "./cert/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+						mysqli_real_connect($con, "xxxx.mysql.database.azure.com", "username", "password", "database", 3306, MYSQLI_CLIENT_SSL);
+						
+						// Do Some MySQL stuff if neccesery, my prefference is to do this in the included PHP files but to start the main connection here (managed from 1 place)
+						//$result = mysqli_query($con,"SELECT * FROM `klanten`");
+						//while ($row = mysqli_fetch_assoc($result)) {
+						//	echo $row['KLANTEN'];
+						//}
 
-					// Filter ID
-					$ID = $_GET['ID'];
-					$ID = (int)preg_replace("/[^0-9]+/", "", $ID);
+						// Filter ID
+						$ID = $_GET['ID'];
+						$ID = (int)preg_replace("/[^0-9]+/", "", $ID);
 
-					//Serve pages, here begins the logic of asigning pages to specific roles
-					if (($ID == "") && ((in_array("user",$rolesAssigned)) || (in_array("admin",$rolesAssigned)))){ //if role user or admin is assigned
-						include './data/userpage.php';
-					} 
-					else if (($ID == 1) && (in_array("admin",$rolesAssigned))){  //if role admin is assigned
-						include './data/adminpage.php';
-					}  
-					else{
-						echo "<br><center><h1>Error</h1></center><br>";;
+						//Serve pages, here begins the logic of asigning pages to specific roles
+						if (($ID == "") && ((in_array("user",$rolesAssigned)) || (in_array("admin",$rolesAssigned)))){ //if role user or admin is assigned
+							include './data/userpage.php';
+						} 
+						else if (($ID == 1) && (in_array("admin",$rolesAssigned))){  //if role admin is assigned
+							include './data/adminpage.php';
+						}  
+						else{
+							echo "<br><center><h1>Error</h1></center><br>";;
+						}
+						
+						//close the connection
+						mysqli_close($con);
 					}
-					
-					//close the connection
-					mysqli_close($con);
+					// Something definitly gone wrong....
+					else{
+						echo "<center>User logged in but token not available....</center>";
+					}
 
 				}
 				//No valid user found, make a redirect to SAML login
